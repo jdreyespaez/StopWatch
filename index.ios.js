e5c4f8977c0493d9e0f4feed50fac7c5ce82fed4
jdreyespaez <jd.reyespaez@gmail.com>
2016-05-09 14:31:42 +0700

    23: Flexbox

--- index.ios.js
+++ index.ios.js
@@ -2,17 +2,30 @@ import React from 'react';
 import {
   Text,
   View,
-  AppRegistry
+  AppRegistry,
+  StyleSheet
 } from 'react-native';
 
 var StopWatch = React.createClass({
   render: function() {
     return <View>
-      <Text>
-        00:00.00
-      </Text>
-      {this.startStopButton()}
-      {this.lapButton()}
+      <View>
+        <View>
+          <Text>
+            00:00.00
+          </Text>
+        </View>
+        <View>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+
+      <View>
+        <Text>
+          I am lis of Laps
+        </Text>
+      </View>
     </View>
   },
   startStopButton: function() {
@@ -28,6 +41,19 @@ var StopWatch = React.createClass({
         Lap
       </Text>
     </View>
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'stretch'
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
   }
 });
 
